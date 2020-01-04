01302c62455d7e98cc19ab8a9885ce6c10bf494b
Gurjot Singh <imasingh@ucdavis.edu>
2020-01-04 10:50:25 -0800

    initial commit for setting up proxy to stress test

--- server.js
+++ server.js
@@ -13,3 +13,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(port, () => {
   console.log(`Proxy listening on port ${port}`);
 });
+
+
+// change this so that it serves the static page
+// which runs the script files, which grabs the bundle files
+// then have this listen to the requests the bundles will make
+// and redirect those requests to the appropriate endpoint--- which in this case would be its local hosts
+// i guess the purpose of this is to test the speeds if the item needs to go through two servers
+
+// by pass the client, the artillery will act as the client
